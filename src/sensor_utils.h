2b4c252e158abc4af0560ed9c7e10ba9afc77364
ngxx-fus <msnp@outlook.com.vn>
2024-12-27 15:16:43 +0700

    Changes: 1. 500ms for user to hold the joystick  before forece exit. It prevent unexpected repeat. 2. Add grid (25%, 50%, 75%, 100%) 3. Add shifted left/right 4. Add increase/decrease bar spacing in chart

--- src/sensor_utils.h
+++ src/sensor_utils.h
@@ -9,18 +9,20 @@
 #define sensors_color_5 0xffff          /// left button text
 #define sensors_color_6 0x6b4d          /// left button background
 #define sensors_color_7 0xffff          /// left button text
-#define sensors_color_8  0x3675         /// humid icon
-#define sensors_color_9  0x3675         /// humid text ["label":"value"]
-#define sensors_color_10 0xdaeb         /// temp icon
-#define sensors_color_11 0xdaeb         /// temp text  ["label":"value"]
-#define sensors_color_12 0x3675         /// humid legend
-#define sensors_color_13 0xdaeb         /// temp legend
+#define sensors_color_8  0x14f2         /// humid icon
+#define sensors_color_9  0x14f2         /// humid text ["label":"value"]
+#define sensors_color_10 0xcb4d         /// temp icon
+#define sensors_color_11 0xcb4d         /// temp text  ["label":"value"]
+#define sensors_color_12 0x14f2         /// humid legend
+#define sensors_color_13 0xcb4d         /// temp legend
 #define sensors_color_14 0x10c3         /// left x-axis (vertical axis)  
 #define sensors_color_15 0x10c3         /// right x-axis (vertical axis)
 #define sensors_color_16 0x10c3         /// bottom y-axis (horizontal axis)
-#define sensors_color_18 0x3675         /// humid
-#define sensors_color_19 0xdaeb         /// temp chart
+#define sensors_color_18 0x14f2         /// humid chart
+#define sensors_color_19 0xcb4d         /// temp chart
+#define sensors_color_20 0xc618         /// grid line
 #define chart_padding    0x4            /// left x-axis <---d---> 1st chart
+#define hold_time_thres  500
 #define sampling_interval 2000          /// dht11 required at leaset 2s
 #define chart_interval   10000           /// dht11 required at leaset 2s
 
@@ -96,17 +98,38 @@ void run_env_info(uint16_t const history_size = 15, uint16_t const column_distan
 
     msg2ser("call\t", "run_env_info");
 
-    uint8_t title0 = 2, humid_row = title0 + 40, temp_row = humid_row + 22,
+    uint8_t title0 = 2, humid_row = title0 + 37, temp_row = humid_row + 20,
             btn0 = 185, chart_row = temp_row + 40;
     static float humid[17], temp[17], latest_humid, latest_temp;
-
+    static uint16_t start_pos = 0, end_pos;
     uint16_t n_lines = history_size, col_d  = column_distance;
+
+    uint64_t last_t = 0;
 
     while(run_show_envinfo){
         
-        /// for zoom :v
-        if( x_adc_value() < 20 ) n_lines = (n_lines>3)?(n_lines-1):(n_lines);
-        else if( x_adc_value() > 200 ) n_lines = (n_lines < history_size)?(n_lines+1):(n_lines);
+        /// for Bar spacing :v
+        if( x_adc_value() < 30 ) {
+            msg2ser("\t", "Bar spacing: +1");
+            n_lines = (n_lines>3)?(n_lines-1):(n_lines);
+            last_t = millis(); while( x_adc_value() < 30 && millis() - last_t < hold_time_thres);
+        }else 
+            if ( x_adc_value() > 190 ) {
+                msg2ser("\t", "Bar spacing: -1");
+                n_lines = (n_lines < history_size)?(n_lines+1):(n_lines);
+                last_t = millis(); while( x_adc_value() > 190 && millis() - last_t < hold_time_thres);
+            }
+        /// for left-right shift
+        if( y_adc_value() < 30 ) {
+            msg2ser("\t", "Shifted: +1 left");
+            start_pos = (start_pos < 16 - n_lines-1) ? (start_pos+1) : start_pos;
+            last_t = millis(); while( y_adc_value() < 30 && millis() - last_t < hold_time_thres);
+        }else 
+            if ( y_adc_value() > 190 ) {
+                msg2ser("\t", "Shifted: +1 right");
+                start_pos = (start_pos > 0) ? (start_pos-1) : start_pos;
+                last_t = millis(); while( y_adc_value() > 190 && millis() - last_t < hold_time_thres);
+            }
         /// re-calc col_d
         if(n_lines < history_size)
             col_d = 150 / n_lines;
@@ -124,8 +147,8 @@ void run_env_info(uint16_t const history_size = 15, uint16_t const column_distan
         if(  millis() - sensors::last_update_chart >= chart_interval ){
             sensors::last_update_chart = millis();
             /// sync to usb serial
-            msg2ser("Humid: ", latest_humid); 
-            msg2ser("Temp:  ", latest_temp);
+            msg2ser("\t", "Humid: ", latest_humid); 
+            msg2ser("\t", "Temp:  ", latest_temp);
             /// shift-left chart
             rept(uint16_t, i, 0, 15)
                 humid[15-i+1] = humid[15-i], temp[15-i+1] = temp[15-i];
@@ -145,19 +168,27 @@ void run_env_info(uint16_t const history_size = 15, uint16_t const column_distan
         /// show temp and humid icon
         canvas.insert_bitmap_image(POINT<>(humid_row, 8), _13x18_humid_icon, 13, 18, sensors_color_8);
         canvas.insert_bitmap_image(POINT<>(temp_row, 8), _13x18_temp_icon, 13, 18, sensors_color_10);
-        /// show x-axis, y-axis
-        canvas.insert_line(POINT<>(chart_row, 5), POINT<>(chart_row+75, 5), sensors_color_14);
-        canvas.insert_line(POINT<>(chart_row, 163), POINT<>(chart_row+75, 163), sensors_color_15);
+        /// show 100% grid
+        canvas.insert_line(POINT<>(chart_row, 5), POINT<>(chart_row, 163), sensors_color_20);
+        /// show 75% grid
+        canvas.insert_line(POINT<>(chart_row+18, 5), POINT<>(chart_row+18, 163), sensors_color_20);
+        /// show 50% grid
+        canvas.insert_line(POINT<>(chart_row+37, 5), POINT<>(chart_row+37, 163), sensors_color_20);
+        /// show 25% grid
+        canvas.insert_line(POINT<>(chart_row+56, 5), POINT<>(chart_row+56, 163), sensors_color_20);
+        /// show y-axis
         canvas.insert_line(POINT<>(chart_row+74, 5), POINT<>(chart_row+74, 163), sensors_color_16);
+        /// show x-axis
+        canvas.insert_line(POINT<>(chart_row, 5), POINT<>(chart_row+74, 5), sensors_color_14);
+        canvas.insert_line(POINT<>(chart_row, 163), POINT<>(chart_row+74, 163), sensors_color_15);
         /// show temp, humid;
-        canvas.insert_text(POINT<>(humid_row+15, 29), String("Humid: ")+String(latest_humid), sensors_color_9);
-        canvas.insert_text(POINT<>(temp_row+15, 29), String("Temp: ")+String(latest_temp), sensors_color_11);
-        
+        canvas.insert_text(POINT<>(humid_row+15, 29), String("Humid: ")+String(latest_humid)+String(" oC"), sensors_color_9);
+        canvas.insert_text(POINT<>(temp_row+15, 29), String("Temp:   ")+String(latest_temp)+String(" %"), sensors_color_11);
         /// show chart
-        uint16_t h;
+        uint16_t h; 
         rept(uint16_t, i, 0, n_lines-1){
             
-            h = max(0.0f, humid[n_lines-1-i]) * 75 / 100.0;
+            h = max(0.0f, humid[start_pos + (n_lines - 1) - i]) * 75 / 100.0;
             rept(uint16_t, d, 6 + chart_padding, 6 + chart_padding + 2)
                 canvas.insert_line(
                     POINT<>(chart_row+75,   d + i*col_d), 
@@ -165,7 +196,7 @@ void run_env_info(uint16_t const history_size = 15, uint16_t const column_distan
                     sensors_color_18
                 );
             
-            h = max(0.0f, temp[n_lines-1-i]) * 75 / 53.0;
+            h = max(0.0f, temp[start_pos + (n_lines - 1) - i]) * 75 / 50.0;
             rept(uint16_t, d, 6 + chart_padding + 4, 6 + chart_padding + 6)
                 canvas.insert_line(
                     POINT<>(chart_row+75,   d + i*col_d), 
@@ -174,12 +205,11 @@ void run_env_info(uint16_t const history_size = 15, uint16_t const column_distan
                 );
         }
         /// show legend (max value)
-        canvas.insert_text(POINT<>(chart_row+3, 5), "100%", 0x3675);
-        canvas.insert_text(POINT<>(chart_row+3, 123), "50oC", 0xdaeb);
+        canvas.insert_text(POINT<>(chart_row - 2, 3), "100%", sensors_color_12);
+        canvas.insert_text(POINT<>(chart_row - 2, 125), "50oC", sensors_color_13);
         /// show changed
         canvas.show();
         /// wait for the joystick released
-        while( x_adc_value() > 200 || x_adc_value() < 30);
     }
 }
 #endif
