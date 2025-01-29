91475606b0c53973ceccd3e867512e987855ec9d
ngxx-fus <msnp@outlook.com.vn>
2025-01-29 08:18:51 +0700

    trying to make the img_utils for quick make img.bin

--- utils/img2bin.cpp
+++ utils/img2bin.cpp
@@ -1,6 +1,144 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include <jpeglib.h>
+// #include <jpeglib.h>
+
+void raise_exception(string msg = "unknown"){
+    cout << "Err: " << msg << endl;
+}
+
+template<class Tvalue>
+class _2d_vct{
+
+private:
+    vector< vector<Tvalue> > data;
+
+public:
+
+    template<class Tsize>
+    _2d_vct(Tsize h = 220U, Tsize w = 172U, Tvalue val = 0U){
+        if(h == 0 || w == 0) return;
+        data.resize(h, vector<Tvalue>(w, val));
+    }
+
+    template<class Tsize>
+    _2d_vct(vector<Tvalue> V_val, Tsize h = 220U, Tsize w = 172U){
+        if(h == 0 || w == 0 || V_val.size() != w*h) {
+            raise_exception("wrong size!");
+            return;
+        }
+        data.resize(h, vector<Tvalue>(w, Tvalue(0)));
+        for(Tsize r = 0; r < w; ++r){
+            for(Tsize c = 0; c < h; ++c){
+                this->data.at(r).at(c) = V_val.at(r * w + c);
+            }
+        }
+    }
+
+    int height(){
+        return this->data.size();
+        }
+
+    int width(){
+        if(this->height() < 1) return 0;
+        return this->data.at(0).size();    
+    }
+
+    bool is_empty(){
+        return (this->height() < 1 || this->width() < 1);
+    }
+
+    template<class Tsize>
+    Tvalue& at(Tsize row, Tsize col){
+        if(row >= height() || col >= width()) {
+        	cout << "? " << "Img-col : " << width() << " Img-row : " << height() << endl;
+        	cout << "? " << "col : " << col << " row : " << row << endl;
+            raise_exception("invalid size!");
+            exit(1);
+        }
+        return data.at(row).at(col);
+    }
+
+    template<class Tsize>
+    _2d_vct operator = (_2d_vct o){
+        this->data.resize(o.height(), vector<Tvalue>(o.width(), 0));
+        for(Tsize r = 0; r < o.height(); ++r){
+            for(Tsize c = 0; c < o.width(); ++c){
+                this->data.at(r).at(c) = o.at(r, c);
+            }
+        }
+        return o;
+    }
+
+    template<class Tsize>
+    _2d_vct add (_2d_vct o, Tsize row_offset = 0, Tsize col_offset = 0){
+        Tsize row_start = row_offset, row_stop = min(height()-1, row_offset+o.height()-1),
+              col_start = col_offset, col_stop = min(width()-1,  col_offset+o.width() -1); 
+        for(Tsize r = row_start; r <= row_stop; ++r){
+            for(Tsize c = col_start; c <= col_stop; ++c){
+                this->data.at(r).at(c) += o.at(r, c);
+            }
+        }
+        return *this;
+    }
+
+    template<class Tsize>
+    _2d_vct operator + (_2d_vct o){
+        if(
+            this->height() != o.height() 
+            || this->width() != o.width()
+        ){
+            raise_exception("wrong size!");
+            return *this;
+        }
+        for(Tsize r = 0; r < o.height(); ++r){
+            for(Tsize c = 0; c < o.width(); ++c){
+                o.at(r, c) += this->data.at(r).at(c);
+            }
+        }
+        return o;
+    }
+
+    template<class Tsize>
+    _2d_vct conv(_2d_vct kernel){
+        if(
+            this->height() > kernel.height() 
+            || this->width() > kernel.width()
+        ){
+            raise_exception("kernel's size > img's size!");
+            return *this;
+        }
+        _2d_vct result(this->height() - kernel.height() +1, this->width() - kernel.width() + 1);
+        for( Tsize r = 0; r < result.height(); ++r){
+            for( Tsize c =  0; c < result.height(); ++c){
+                uint64_t pixel = 0;
+                for( Tsize kr = 0; kr < kernel.height(); ++kr){
+                    for(Tsize kc = 0; kc < kernel.width(); ++kc){
+                        pixel += this->data.at(r+kr, c+kc)*kernel.at(kr, kc);
+                    }
+                }
+                pixel /= (kernel.height() * kernel.width());
+                result.at(r, c) = pixel;
+            }
+        } 
+        return result;
+    }
+
+};
+
+
+class img_16bit{
+public:
+    template<class Tsize>
+    img_16bit(Tsize w = 172U, Tsize h = 220U, uint16_t color = 0){
+        _16bit_img.resize(h, vector<uint16_t>(w, color));
+    }
+    clear(){
+        _16bit_img.resize(h, vector<uint16_t>(w, color));
+    }
+    resize(){
+
+    }
+}
 
 uint16_t rgb_to_rgb565(unsigned char r, unsigned char g, unsigned char b) {
     // Extract 5 bits for red, 6 bits for green, and 5 bits for blue
@@ -75,6 +213,7 @@ void readJPEG(const char* filename, const char* out) {
 }
 
 int main(int argc, char* argv[]){
+
     for(int i = 0; i < argc; i++)
         cout << "arg[" << i << "] : " << argv[i] << endl;
     if(argc != 3) return 0;
