702f27acfe8cbce9f5fe332eef780f1e27da3956
ngxx-fus <msnp@outlook.com.vn>
2025-02-03 10:15:58 +0700

    changed

--- .pio/build/esp32doit-devkit-v1/FrameworkArduino/USB.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/USB.cpp.o: \
- /home/ngxxfus/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
- /home/ngxxfus/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- /home/ngxxfus/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
